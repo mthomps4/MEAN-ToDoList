fdb5a65f28daf6686b6493af16e24f0b733690f8
Matthew <matthew.thompson.a@gmail.com>
2016-06-20 20:57:53 -0400

    uploaded Refresher Notes for later use.

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
 var express = require('express');
-
+var todos = require('../../public/mock/todos.json');
 var router = express.Router();
 
 router.get('/todos', function(req, res){
-  res.json({todos:[]});
+  res.json({todos:todos});
 });
 
 // ADD POST ROUTE FOR NEW POSTS
 // ADD PUT ROUTE TO UPDATE EXISTING POSTS
 // ADD DELETE ROUTE TO REMOVE POSTS
 
-module.exports = router; 
+module.exports = router;
